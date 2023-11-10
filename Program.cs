f5ba424e553bcfe1bc7824595c598400506741dc
Cristover Wurangian <christwurangian@gmail.com>
2023-11-10 10:41:52 +0700

    remove json console

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Nodes;
 using HtmlAgilityPack;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Logging.AddJsonConsole();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
